40a771e866be9cb805c9e8003f85aafa88175b9f
OEscriba <fernadohtr@gmail.com>
2024-07-19 22:30:35 -0300

    feat: creates Java project simulating a bank

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente fernando = new Cliente();
+        fernando.setNome("Fernando");
+
+        Conta cc = new ContaCorrente(fernando);
+        Conta poupanca = new ContaPoupanca(fernando);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+        
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
