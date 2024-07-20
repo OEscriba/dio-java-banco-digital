d7b7eacb9a27cc207cf607cf9c73211d735ac71a
OEscriba <fernadohtr@gmail.com>
2024-07-19 22:49:09 -0300

    feat: add date of birth to Cliente.java and creates its appropriate constructor

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,14 +1,39 @@
 package cliente;
+
+import java.time.LocalDate;
+
 public class Cliente {
     //Atributos
     public String nome;
+    public String cpf;
+    public LocalDate dataNascimento;
 
+    // Constructor
+    public Cliente(String nome, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    //Getters e Setters
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
--- src/operacoes/Movimentacoes.java
+++ src/operacoes/Movimentacoes.java
@@ -1,4 +1,6 @@
 package operacoes;
+import java.time.LocalDate;
+
 import cliente.Cliente;
 import conta.Conta;
 import conta.ContaCorrente;
@@ -6,8 +8,7 @@ import conta.ContaPoupanca;
 
 public class Movimentacoes {
     public static void main(String[] args) {
-        Cliente fernando = new Cliente();
-        fernando.setNome("Fernando");
+        Cliente fernando = new Cliente("Fernando Thibes", "123.456.999-99", LocalDate.of(2000, 3, 25));
 
         Conta cc = new ContaCorrente(fernando);
         Conta poupanca = new ContaPoupanca(fernando);
